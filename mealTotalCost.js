2aa741df378668f625fbc9697ac5796785b80c00
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2023-03-14 13:20:10 +0100

    Added mealTotalCost file

--- mealTotalCost.js
+++ mealTotalCost.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// Given the meal price (base cost of a meal), tip percent
+// (the percentage of the meal price being added as tip), and tax percent
+// (the percentage of the meal price being added as tax) for a meal, find and print
+// the meal's total cost. Round the result to the nearest integer.
+
+function solve(meal_cost, tip_percent, tax_percent) {
+  // Write your code here
+  const tip_cost = meal_cost * (tip_percent / 100);
+  const tax_cost = meal_cost * (tax_percent / 100);
+  const totalCost = meal_cost + tip_cost + tax_cost;
+  console.log(Math.round(totalCost));
+}
+solve(10.25, 17, 5);
